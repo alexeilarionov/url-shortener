32966765aa45de38617ee356a1aab6166758234c
Aleksei Larionov <github@lari4.com>
2023-06-04 19:16:34 +0100

    fix return url, not id

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -22,26 +22,33 @@ func handler(res http.ResponseWriter, req *http.Request) {
 			http.Error(res, "Failed to read request body", http.StatusBadRequest)
 			return
 		}
-		//if len(body) == 0 {
-		//	http.Error(res, "Empty request body", http.StatusBadRequest)
-		//	return
-		//}
+		scheme := "http"
+		if req.TLS != nil {
+			scheme = "https"
+		}
+
+		if len(body) == 0 {
+			http.Error(res, "Empty request body", http.StatusBadRequest)
+			return
+		}
 		encoded := app.Encode(body)
 		data[encoded] = string(body)
 		res.Header().Set("content-type", "text/plain")
 		res.WriteHeader(http.StatusCreated)
-		res.Write([]byte(encoded))
+		url := scheme + "://" + req.Host + "/" + encoded
+		res.Write([]byte(url))
 
 	} else if req.Method == http.MethodGet {
 		fmt.Println(req.URL)
 		path := req.URL.Path
-		//if len(path) < 2 {
-		//	http.Error(res, "Bad request", http.StatusBadRequest)
-		//	return
-		//}
+		if len(path) < 2 {
+			http.Error(res, "Bad request", http.StatusBadRequest)
+			return
+		}
 		path = path[1:]
 		if _, ok := data[path]; ok {
-			http.Redirect(res, req, data[path], http.StatusTemporaryRedirect)
+			res.Header().Set("Location", data[path])
+			res.WriteHeader(http.StatusTemporaryRedirect)
 		}
 	} else {
 		http.Error(res, "Not supported", http.StatusBadRequest)
