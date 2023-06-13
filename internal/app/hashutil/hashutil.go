16174f3cd6415c3c8997b6adf5f748537860e187
Aleksei Larionov <github@lari4.com>
2023-06-13 10:12:05 +0100

    fix bug with containing "/" in shorten string, using url save base64 encoding

--- internal/app/hashutil/hashutil.go
+++ internal/app/hashutil/hashutil.go
@@ -7,7 +7,7 @@ import (
 
 func Encode(data []byte) string {
 	hash := md5.Sum(data)
-	base64Hash := base64.StdEncoding.EncodeToString(hash[:])
+	base64Hash := base64.RawURLEncoding.EncodeToString(hash[:])
 	shortHash := base64Hash[:7]
 	return shortHash
 }
